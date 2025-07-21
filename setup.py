30dc95667f2fb47309f5e6d11d696bdf26bfbb68
Paul Haesler <linkjuggler@gmail.com>
2025-07-21 11:45:11 +1000

    Rebadge and cleanup setup.py.

--- setup.py
+++ setup.py
@@ -31,14 +31,7 @@ build_cfg = dict(
 print(build_cfg)
 
 extensions = [
-    Extension('hdstats.geomedian', ['hdstats/geomedian.pyx'], **build_cfg),
     Extension('hdstats.pcm', ['hdstats/pcm.pyx'], **build_cfg),
-    Extension('hdstats.ts', ['hdstats/ts.pyx'], **build_cfg),
-    Extension('hdstats.dtw', ['hdstats/dtw.pyx'], **build_cfg),
-    Extension('hdstats.truncated', ['hdstats/truncated.pyx', 'hdstats/randomkit.c'],
-              depends=['hdstats/randomkit.h'], **build_cfg),
-    Extension('hdstats.wishart', ['hdstats/wishart.pyx', 'hdstats/randomkit.c'],
-              depends=['hdstats/randomkit.h'], **build_cfg),
 ]
 
 tests_require = [
@@ -47,21 +40,21 @@ tests_require = [
 ]
 
 setup(
-    name="hdstats",
+    name="geomad",
     packages=find_packages(".", exclude=['tests']),
     include_package_data=True,
     package_data={'': ['hdstats/*.pyx', 'hdstats/*.pyx', 'hdstats/*.h', 'hdstats/*.c']},
-    setup_requires=["Cython>=0.23", "numpy", "scipy"],
-    install_requires=["numpy", "scipy", "astropy"],
+    setup_requires=["Cython>=0.29", "numpy<2", "scipy<1.15"],
+    install_requires=["numpy<2", "scipy<1.15", "astropy"],
     tests_require=tests_require,
     extras_require={
         'test': tests_require,
     },
-    version="0.1.8.post1",
-    description="High-dimensional statistics.",
-    url="http://github.com/daleroberts/hdstats",
-    author="Dale Roberts",
-    author_email="dale.o.roberts@gmail.com",
+    version="1.0.0-rc1",
+    description="Geomad.",
+    url="http://github.com/GeoscienceAustralia/geomad",
+    author="GeoscienceAustralia",
+    author_email="",
     license="Apache 2.0",
     zip_safe=False,
     ext_modules=extensions
